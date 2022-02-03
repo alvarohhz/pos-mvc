ee48871122a5c73fab35c2def063bc2680037552
Alpha <56811783+alvarohhz@users.noreply.github.com>
2022-02-03 21:28:59 +0700

    asdf

--- tests/logintest.php
+++ tests/logintest.php
@@ -3,6 +3,8 @@
 // Path to run ./vendor/bin/phpunit --bootstrap vendor/autoload.php FileName.php
 // Butuh Framework PHPUnit
 use PHPUnit\Framework\TestCase;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Chrome\ChromeDriver;
 
 // Class yang mau di TEST.
 require_once "logintest";
@@ -10,5 +12,15 @@ require_once "logintest";
 
 class logintest extends TestCase
 {
-    
-}
+   
+   public $capabilities;
+   public function __construct()
+        {
+        $this->capabilities = DesiredCapabilities::chrome();
+    // Disable accepting SSL certificates
+    // selenium-server-standalone-#.jar (version 3.x)
+    // $host = 'http://localhost:4455/wd/hub'
+    $this->webDriver = RemoteWebDriver::create($host, $capabilities);
+        }}
+
+        
